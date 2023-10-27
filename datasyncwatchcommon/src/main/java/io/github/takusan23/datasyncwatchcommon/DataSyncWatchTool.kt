5b4265023de9f04576ab17d3dd62d539a5ce57fd
takusan23 <takumu1200@gmail.com>
2023-10-28 02:51:45 +0900

    初回時に取得する

--- datasyncwatchcommon/src/main/java/io/github/takusan23/datasyncwatchcommon/DataSyncWatchTool.kt
+++ datasyncwatchcommon/src/main/java/io/github/takusan23/datasyncwatchcommon/DataSyncWatchTool.kt
@@ -3,6 +3,7 @@ package io.github.takusan23.datasyncwatchcommon
 import android.content.Context
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEvent
+import com.google.android.gms.wearable.DataItem
 import com.google.android.gms.wearable.DataMapItem
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.Wearable
@@ -38,21 +39,30 @@ object DataSyncWatchTool {
      * @param context [Context]
      */
     fun receiveDataSync(context: Context) = callbackFlow {
+
+        fun sendResult(dataItem: DataItem) {
+            when (dataItem.uri.path) {
+                PATH -> {
+                    // PutDataMapRequest を復元する
+                    val dataMap = DataMapItem.fromDataItem(dataItem).dataMap
+                    // キーを使って値を取り出す
+                    trySend(dataMap.getString(TEXT_KEY))
+                }
+            }
+        }
+
         val listener = DataClient.OnDataChangedListener { dataEvents ->
             dataEvents
                 .filter { it.type == DataEvent.TYPE_CHANGED }
-                .forEach { event ->
-                    val dataItem = event.dataItem
-                    when (dataItem.uri.path) {
-                        PATH -> {
-                            // PutDataMapRequest を復元する
-                            val dataMap = DataMapItem.fromDataItem(dataItem).dataMap
-                            // キーを使って値を取り出す
-                            trySend(dataMap.getString(TEXT_KEY))
-                        }
-                    }
-                }
+                .forEach { event -> sendResult(event.dataItem) }
         }
+
+        // 同期的に一回目は取得
+        Wearable.getDataClient(context).dataItems.await()
+            .let { buffer -> (0 until buffer.count).map { buffer.get(it) } }
+            .forEach { dataItem -> sendResult(dataItem) }
+
+        // アプリ起動中に変化した場合
         Wearable.getDataClient(context).addListener(listener)
         awaitClose { Wearable.getDataClient(context).removeListener(listener) }
     }
